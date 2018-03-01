f395120033463d677d4fdaa4df34a48d75c95f08
Morgan Shepard <morgan@Morgans-MacBook-Pro.local>
2018-03-01 16:21:59 -0500

    added a lot of Carl Bratadanadeluski.

--- main.js
+++ main.js
@@ -5,19 +5,24 @@ var boxleft=200;
             
 document.addEventListener('keydown', (event) => {
   const keyName = event.key;
-  document.getElementById("box").style.top = boxtop
-  document.getElementById("box").style.left = boxleft
+  document.getElementById("box").style.top = boxtop;
+  document.getElementById("box").style.left = boxleft;
     if (keyName === "ArrowUp") {
-       boxtop + "100px";
+       boxtop -= 20;
+       document.getElementById("box").style.top = boxtop + "px";
     }
     if (keyName === "ArrowDown") {
-        boxtop - "100px";
+        boxtop += 20;
+        document.getElementById("box").style.top = boxtop + "px";
+
     }
-    if (keyName === "ArrowUp") {
-        boxleft + "100px";
+    if (keyName === "ArrowRight") {
+        boxleft += 20;
+        document.getElementById("box").style.left = boxleft + "px";
     }
     if (keyName === "ArrowLeft") {
-        boxleft - "100px";
+        boxleft -= 20;
+        document.getElementById("box").style.left = boxleft + "px";
     }
 
 
